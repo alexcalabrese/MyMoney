712a98cf3ab74c67682dbc74e68ee3a4641c6a3a
Alex Calabrese <alex@moveforwardgroup.it>
2021-10-15 20:12:20 +0200

    New start

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'transactions' => TransactionController::class,
+    'transactionTypes' => TransactionTypeController::class,
+    'categories' => Category::class
+]);
