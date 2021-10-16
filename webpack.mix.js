77567a431d1c5bae00a32ab8563c574262d50297
Alex Calabrese <alex@moveforwardgroup.it>
2021-10-16 20:44:18 +0200

    Fixed docker mysql image

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,11 +13,11 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue({ version: 3 })
-    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css');
 
-    mix.webpackConfig({
-        devServer: {
-            host: '0.0.0.0',
-            port: 8080,
-        },
-    });
+mix.webpackConfig({
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+    },
+});
