244b6481b924a3340e47cd99240ad3296fd54791
Alex Calabrese <alex@moveforwardgroup.it>
2021-10-22 19:02:01 +0200

    Added and customized sakai theme

--- resources/js/src/App.vue
+++ resources/js/src/App.vue
@@ -35,91 +35,20 @@ export default {
             mobileMenuActive: false,
             menu : [
                 {
-                    label: 'Home',
+                    // label: 'Home',
                     items: [{
                         label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/'
                     }]
                 },
 				{
-					label: 'UI Kit', icon: 'pi pi-fw pi-sitemap',
+					// label: 'UI Kit', icon: 'pi pi-fw pi-sitemap',
 					items: [
-						{label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/formlayout'},
-						{label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/input'},
-                        {label: "Float Label", icon: "pi pi-fw pi-bookmark", to: "/floatlabel"},
-                        {label: "Invalid State", icon: "pi pi-fw pi-exclamation-circle", to: "invalidstate"},
-						{label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/button'},
-						{label: 'Table', icon: 'pi pi-fw pi-table', to: '/table'},
-						{label: 'List', icon: 'pi pi-fw pi-list', to: '/list'},
-						{label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/tree'},
-						{label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/panel'},
-						{label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/overlay'},
-						{label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/menu'},
-						{label: 'Message', icon: 'pi pi-fw pi-comment', to: '/messages'},
-						{label: 'File', icon: 'pi pi-fw pi-file', to: '/file'},
-						{label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/chart'},
-						{label: 'Misc', icon: 'pi pi-fw pi-circle-off', to: '/misc'},
+						{label: 'Account', icon: 'pi pi-fw pi-user', to: '/account'},
+						{label: 'Transactions', icon: 'pi pi-fw pi-money-bill', to: '/transactions'},
+						{label: 'Statistics', icon: 'pi pi-fw pi-chart-bar', to: '/statistics'},
+						{label: 'Import', icon: 'pi pi-fw pi-file-excel', to: '/import'},
 					]
 				},
-				{
-					label: 'Pages', icon: 'pi pi-fw pi-clone',
-					items: [
-						{label: 'Crud', icon: 'pi pi-fw pi-user-edit', to: '/crud'},
-						{label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/timeline'},
-						{label: 'Empty', icon: 'pi pi-fw pi-circle-off', to: '/empty'}
-					]
-				},
-                {
-                    label: 'Menu Hierarchy', icon: 'pi pi-fw pi-search',
-                    items: [
-                        {
-                            label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {
-                                    label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        {label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark'},
-                                    ]
-                                },
-                                {
-                                    label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        {label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 1.2.2', icon: 'pi pi-fw pi-bookmark'}
-                                    ]
-                                },
-                            ]
-                        },
-                        {
-                            label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                {
-                                    label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        {label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 2.1.3', icon: 'pi pi-fw pi-bookmark'},
-                                    ]
-                                },
-                                {
-                                    label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        {label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 2.2.2', icon: 'pi pi-fw pi-bookmark'}
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    label: 'Get Started', 
-                    items: [
-                        {label: 'Documentation', icon: 'pi pi-fw pi-question', command: () => {window.location = "#/documentation"}},
-                        {label: 'View Source', icon: 'pi pi-fw pi-search', command: () => {window.location = "https://github.com/primefaces/sakai-vue"}}
-                    ]
-                }
             ]
         }
     },
--- resources/js/src/components/Dashboard.vue
+++ resources/js/src/components/Dashboard.vue
@@ -4,11 +4,11 @@
 			<div class="card mb-0">
 				<div class="flex justify-content-between mb-3">
 					<div>
-						<span class="block text-500 font-medium mb-3">Orders</span>
+						<span class="block text-500 font-medium mb-3">Costs</span>
 						<div class="text-900 font-medium text-xl">152</div>
 					</div>
-					<div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width:2.5rem;height:2.5rem">
-						<i class="pi pi-shopping-cart text-blue-500 text-xl"></i>
+					<div class="flex align-items-center justify-content-center bg-pink-800 border-round" style="width:2.5rem;height:2.5rem">
+						<i class="pi pi-shopping-cart text-pink-500 text-xl"></i>
 					</div>
 				</div>
 				<span class="text-green-500 font-medium">24 new </span>
@@ -19,11 +19,11 @@
 			<div class="card mb-0">
 				<div class="flex justify-content-between mb-3">
 					<div>
-						<span class="block text-500 font-medium mb-3">Revenue</span>
+						<span class="block text-500 font-medium mb-3">Earnings</span>
 						<div class="text-900 font-medium text-xl">$2.100</div>
 					</div>
-					<div class="flex align-items-center justify-content-center bg-orange-100 border-round" style="width:2.5rem;height:2.5rem">
-						<i class="pi pi-map-marker text-orange-500 text-xl"></i>
+					<div class="flex align-items-center justify-content-center bg-green-800 border-round" style="width:2.5rem;height:2.5rem">
+						<i class="pi pi-euro text-green-500 text-xl"></i>
 					</div>
 				</div>
 				<span class="text-green-500 font-medium">%52+ </span>
@@ -34,26 +34,11 @@
 			<div class="card mb-0">
 				<div class="flex justify-content-between mb-3">
 					<div>
-						<span class="block text-500 font-medium mb-3">Customers</span>
-						<div class="text-900 font-medium text-xl">28441</div>
-					</div>
-					<div class="flex align-items-center justify-content-center bg-cyan-100 border-round" style="width:2.5rem;height:2.5rem">
-						<i class="pi pi-inbox text-cyan-500 text-xl"></i>
-					</div>
-				</div>
-				<span class="text-green-500 font-medium">520  </span>
-				<span class="text-500">newly registered</span>
-			</div>
-		</div>
-		<div class="col-12 lg:col-6 xl:col-3">
-			<div class="card mb-0">
-				<div class="flex justify-content-between mb-3">
-					<div>
-						<span class="block text-500 font-medium mb-3">Comments</span>
+						<span class="block text-500 font-medium mb-3">Balance</span>
 						<div class="text-900 font-medium text-xl">152 Unread</div>
 					</div>
-					<div class="flex align-items-center justify-content-center bg-purple-100 border-round" style="width:2.5rem;height:2.5rem">
-						<i class="pi pi-comment text-purple-500 text-xl"></i>
+					<div class="flex align-items-center justify-content-center bg-purple-800 border-round" style="width:2.5rem;height:2.5rem">
+						<i class="pi pi-wallet text-purple-500 text-xl"></i>
 					</div>
 				</div>
 				<span class="text-green-500 font-medium">85 </span>
@@ -61,19 +46,30 @@
 			</div>
 		</div>
 
+	<div class="col-12 lg:col-6 xl:col-3">
+		<div class="card pb-8">
+			<h6 class="align-self-center">Current Month:</h6>
+			<Calendar v-model="currentMonth" view="month" dateFormat="mm/yy" :yearNavigator="true" yearRange="2000:2030" showIcon />
+		</div>
+	</div>
 	<div class="col-12 xl:col-6">
 		<div class="card">
-			<h5>Recent Sales</h5>
+			<h5>Sales Overview</h5>
+			<Chart type="line" :data="lineData" />
+		</div>
+	</div>
+	<div class="col-12 xl:col-6">
+		<div class="card">
+			<h5>Sales Overview</h5>
+			<Chart type="line" :data="lineData" />
+		</div>
+	</div>
+	<div class="col-12 xl:col-12">
+		<div class="card">
+			<h5>Latest Transactions</h5>
 			<DataTable :value="products" :rows="5" :paginator="true" responsiveLayout="scroll">
-				<Column style="width:15%">
-					<template #header>
-						Image
-					</template>
-					<template #body="slotProps">
-						<img :src="'images/product/' + slotProps.data.image" :alt="slotProps.data.image" width="50" class="shadow-2" />
-					</template>
-				</Column>
 				<Column field="name" header="Name" :sortable="true" style="width:35%"></Column>
+				<Column field="category" header="Name" :sortable="true" style="width:35%"></Column>
 				<Column field="price" header="Price" :sortable="true" style="width:35%">
 					<template #body="slotProps">
 						{{formatCurrency(slotProps.data.price)}}
@@ -174,10 +170,7 @@
 		</div>
 	</div>
 	<div class="col-12 xl:col-6">
-		<div class="card">
-			<h5>Sales Overview</h5>
-			<Chart type="line" :data="lineData" />
-		</div>
+		
 		<div class="card">
 			<div class="flex align-items-center justify-content-between mb-4">
 				<h5>Notifications</h5>
@@ -225,18 +218,6 @@
 				</li>
 			</ul>
 		</div>
-		<div class="px-4 py-5 shadow-2 flex flex-column md:flex-row md:align-items-center justify-content-between mb-3" 
-			style="border-radius: 1rem; background: linear-gradient(0deg, rgba(0, 123, 255, 0.5), rgba(0, 123, 255, 0.5)), linear-gradient(92.54deg, #1C80CF 47.88%, #FFFFFF 100.01%)">
-			<div>
-				<div class="text-blue-100 font-medium text-xl mt-2 mb-3">TAKE THE NEXT STEP</div>
-				<div class="text-white font-medium text-5xl">Try PrimeBlocks</div>
-			</div>
-			<div class="mt-4 mr-auto md:mt-0 md:mr-0">
-				<a href="https://www.primefaces.org/primeblocks-vue" class="p-button font-bold px-5 py-3 p-button-warning p-button-rounded p-button-raised">
-					Get Started
-				</a>
-			</div>
-		</div>
 	</div>
 	</div>
 </template>
@@ -247,6 +228,7 @@ import ProductService from '../service/ProductService';
 export default {
 	data() {
 		return {
+			currentMonth: null,
 			products: null,
 			lineData: {
 				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -278,6 +260,8 @@ export default {
 	productService: null,
 	created() {
 		this.productService = new ProductService();
+		this.currentMonth = this.getCurrentMonth();
+		console.log(this.currentMonth);
 	},
 	mounted() {
 		this.productService.getProductsSmall().then(data => this.products = data);
@@ -285,6 +269,10 @@ export default {
 	methods: {
 		formatCurrency(value) {
 			return value.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+		},
+		getCurrentMonth(){
+			const today = new Date();
+			return today.getMonth() + "/" + today.getFullYear();
 		}
 	}
 }
